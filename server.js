faa0cda2f3d0d32ad6c95d4a201772df5149293a
PriyankaraUser <priyankarasaman123@gmail.com>
2022-09-21 19:47:45 +0530

    image upload and delete

--- server.js
+++ server.js
@@ -20,6 +20,9 @@ app.use(fileUpload({
 
 app.use('/user', require('./routes/userRouter'))
 app.use('/api', require('./routes/categoryRouter'))
+app.use('/api', require('./routes/upload'))
+// app.use('/api', require('./routes/productRouter'))
+// app.use('/api', require('./routes/paymentRouter'))
 
 
 //connect to DB
@@ -29,9 +32,6 @@ mongoose.connect(DB_URL)
 })
 .catch((err) => console.log('DB connection error', err));
 
-// app.get('/', (req,res)=>{
-//     res.json({msg: "I am Priyankara"}) 
-// })
 
 
 app.listen(PORT, ()=>{
